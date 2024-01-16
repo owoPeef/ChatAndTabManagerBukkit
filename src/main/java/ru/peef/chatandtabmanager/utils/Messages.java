add6db67b6b14747fb8925022b4cdb3719dcf736
owoPeef <owoPeef@gmail.com>
2024-01-16 15:23:48 +0300

    Добавлены новые параметры для отображения.

--- src/main/java/ru/peef/chatandtabmanager/utils/Messages.java
+++ src/main/java/ru/peef/chatandtabmanager/utils/Messages.java
@@ -5,24 +5,32 @@ import org.bukkit.entity.Player;
 
 public class Messages {
     public static String formatMessage(String message, Player player) {
-        return message.replace("{player_nickname}", player.getName())
-                .replace("В§", "§")
+        return message
+                .replace("{player_deaths}", String.valueOf(player.getStatistic(Statistic.DEATHS)))
+                .replace("{player_mobs_killed}", String.valueOf(player.getStatistic(Statistic.MOB_KILLS)))
+                .replace("{player_health}", String.valueOf(player.getHealth()))
+                .replace("{player_level}", String.valueOf(player.getLevel()))
+                .replace("{player_nickname}", player.getName())
                 .replace("&", "§");
     }
 
     public static String formatMessage(String original_message, String message, Player player) {
         return original_message.replace("{player_message}", message)
+                .replace("{player_deaths}", String.valueOf(player.getStatistic(Statistic.DEATHS)))
+                .replace("{player_mobs_killed}", String.valueOf(player.getStatistic(Statistic.MOB_KILLS)))
+                .replace("{player_health}", String.valueOf(player.getHealth()))
+                .replace("{player_level}", String.valueOf(player.getLevel()))
                 .replace("{player_nickname}", player.getName())
-                .replace("&", "§")
-                .replace("В§", "§");
+                .replace("&", "§");
     }
 
     public static String formatMessage(String original_message, Player player, boolean NeedExtension) {
         return original_message
                 .replace("{player_deaths}", String.valueOf(player.getStatistic(Statistic.DEATHS)))
+                .replace("{player_mobs_killed}", String.valueOf(player.getStatistic(Statistic.MOB_KILLS)))
+                .replace("{player_health}", String.valueOf(player.getHealth()))
                 .replace("{player_level}", String.valueOf(player.getLevel()))
                 .replace("{player_nickname}", player.getName())
-                .replace("&", "§")
-                .replace("В§", "§");
+                .replace("&", "§");
     }
 }
